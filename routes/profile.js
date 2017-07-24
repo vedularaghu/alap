62867cb3853ef6fa07a3975246762a6e80c26378
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-07-24 16:28:46 +0530

    Add routes for posts

---
+++ routes/profile.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * View a profile
+ */
+router.get('/:id', (req, res) => {
+
+});
+
+/**
+ * View own profile
+ */
+router.get('/me', (req, res) => {
+
+});
